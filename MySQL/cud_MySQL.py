96e4f60578a1761fcccd1274ce2a3b8add42018c
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2020-12-17 00:32:50 -0800

    CUD MySQL

--- MySQL/cud_MySQL.py
+++ MySQL/cud_MySQL.py
@@ -2,8 +2,9 @@ import pymysql
 
 conn = pymysql.connect(host='localhost', user='root', password='', database='jing_dong')
 cur = conn.cursor()
-sql = "insert into goods values(null, 'bag3', 1, 8, 66, 1, 0)"
-ret = cur.execute(sql)
+#sql = "insert into goods values(null, 'bag3', 1, 8, 66, 1, 0)"
+sql1 = 'update goods set price = 68 where id = 22'
+ret = cur.execute(sql1)
 conn.commit()
 print(ret)
 cur.close()
